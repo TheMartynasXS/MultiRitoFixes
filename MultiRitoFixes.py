eaccc137ebb10f4f23d16895a3fd4a7c5444a5a1
TheMartynasXS <Martynasxs@proton.me>
2025-03-03 15:31:39 +0200

    rewrite

--- MultiRitoFixes.py
+++ MultiRitoFixes.py
@@ -3,34 +3,31 @@ import sys
 from sys import argv
 from os import path
 import re
-# import fnv1a
 from xxhash import xxh64
 from zipfile import ZIP_DEFLATED, ZipFile
-# from LtMAO.binfile import BIN, BINField, BINType
-# from LtMAO.wadfile import WAD, WADChunk
 from pyritofile import BIN,WAD,WADChunk, BINField, BINType, BINEntry
 from pyritofile.structs import Vector, Matrix4
+import tempfile
+import json
 
 if len(argv) < 2:
-    input('Plz drag and drop a folder ')
+    input('Do not run this program directly,\ndrag and drop file/folder on to the fixer')
     exit()
+else :
+    print(f"Running on: {argv[1]}")
+
 
 def compute_hash(s: str):
     if s.startswith("0x"):
-        return s.removeprefix("0x")
+        return s[2:]
     
-    h = 0x811c9dc5 
-    for b in s.encode('ascii').lower(): 
-        h = ((h ^ b) * 0x01000193) % 0x100000000 
-    return f"{h:08x}"
+    h = 0x811c9dc5
+    for b in s.encode('ascii').lower():
+        h = (h ^ b) * 0x01000193
+    return f"{h & 0xffffffff:08x}"
 
 filesInWad = set()
-
-
 class CACHED_BIN_HASHES(dict):
-    """
-    Simple class that stores our hashed strings without needing to manually make the hash to get it
-    """
     def __getitem__(self, key):
         if key in self.keys():
             return super().__getitem__(key)
@@ -49,9 +46,8 @@ if path.exists(allowed_chars):
     with open(allowed_chars, 'r') as file:
         allowed_chars = [line.strip() for line in file.readlines()]
 cached_bin = CACHED_BIN_HASHES()
-inpt = argv[1]
-def parse_bin(bin_path, bin_file):
-    print(f"Reading: {bin_path}")
+input_path = argv[1]
+def parse_bin(bin_path, bin_file, is_standalone=False):
     for entry in bin_file.entries:
         if entry.type == cached_bin['SkinCharacterDataProperties']:
             is_champion = False
@@ -97,16 +93,16 @@ def parse_bin(bin_path, bin_file):
                         elif tName != -1 and sName != -1 and tNameIsPath:
                             sampler_def.data[tName].hash = cached_bin['TexturePath']
                             sampler_def.data[sName].hash = cached_bin['TextureName']
-
-        if entry.type != cached_bin['ResourceResolver']:
+    for entry in bin_file.entries:
+        if entry.type != cached_bin['ResourceResolver'] and (not(is_standalone)):
             for item in entry.data:
-                item.data = searchForStringTypeValues(item.data)             
+                item.data = traverse_bin(item.data)             
     return bin_file
 
 bin_assets = set()
-def ddstotex(obj):
+def dds_to_tex(obj):
     if obj.lower().endswith(".dds"):
-        pattern = re.compile(r"assets/(characters/[a-d])", re.IGNORECASE)
+        pattern = re.compile(r"assets/(characters/[a-j])", re.IGNORECASE)
         check = (re.match(pattern, obj) != None)
         if check and (not (xxh64(obj.lower()).hexdigest()  in filesInWad)):
             ext = re.compile(r"\.dds$", re.IGNORECASE)
@@ -115,53 +111,27 @@ def ddstotex(obj):
     return obj
 
 #recursively search for string type values
-def searchForStringTypeValues(obj):
-    if type(obj) == list:
-        for item in obj:
-            item = searchForStringTypeValues(item)
-        pass
-    elif type(obj) == str:
-        obj = ddstotex(obj)
-        # ddstotex(obj)
-        # if obj.lower().endswith(".dds"):
-        #     pattern = re.compile(r"assets/(characters/[a-d])", re.IGNORECASE)
-        #     check = re.match(pattern, obj) != None
-        #     if check and not(xxh64(obj.lower()).hexdigest() in filesInWad):
-        #         ext = re.compile(r"\.dds$", re.IGNORECASE)
-        #         print(f"Couldn't find {obj} in the wad renaming to .tex")
-        #         # obj = re.sub(ext, ".tex", obj)
-        #     else:
-        #         print(f"{check} {xxh64(obj.lower()).hexdigest()}")
-        # elif obj.lower().endswith(".tex"):
-        #     ext = re.compile(r"\.tex$", re.IGNORECASE)
-        #     if xxh64(re.sub(ext, ".dds", obj.lower())).hexdigest() in filesInWad:
-        #         pass
-        #         print(f"Found {obj} in the wad renaming to .dds")
-        #         # obj = re.sub(ext, ".dds", obj)
-    elif type(obj) == BINField and obj.type == BINType.String:
-        obj.data = ddstotex(obj.data)
-        # ddstotex(obj.data)
+def traverse_bin(obj):
+    if isinstance(obj, str):
+        return dds_to_tex(obj)
 
-                # obj.data = re.sub(ext, ".tex", obj.data)
-    elif type(obj) == BINField and obj.type == BINType.List:
-        for listitem in obj.data:
-            if hasattr(listitem, 'data'):
-                listitem.data = searchForStringTypeValues(listitem.data)
-            else:
-                listitem = searchForStringTypeValues(listitem)
-    elif type(obj) == BINField and obj.type == BINType.List2:
-        obj.data = searchForStringTypeValues(obj.data)
-        pass
-    elif type(obj) == BINField and obj.type == BINType.Embed:
-        obj.data = searchForStringTypeValues(obj.data)
-        pass
-    elif type(obj) == BINField and obj.type == BINType.Pointer:
-        obj.data = searchForStringTypeValues(obj.data)
-        pass
-    else:
-        pass
-        # if(hasattr(obj, 'type')):
-        #     print(obj.type)
+    if isinstance(obj, list):
+        for i in range(len(obj)):
+            obj[i] = traverse_bin(obj[i])
+        return obj
+    
+    if isinstance(obj, BINField):
+        if obj.type == BINType.String:
+            obj.data = dds_to_tex(obj.data)
+        elif obj.type == BINType.List:
+            for i in range(len(obj.data)):
+                if hasattr(obj.data[i], 'data'):
+                    obj.data[i].data = traverse_bin(obj.data[i].data)
+                else:
+                    obj.data[i] = traverse_bin(obj.data[i])
+        elif obj.type in (BINType.List2, BINType.Embed, BINType.Pointer):
+            obj.data = traverse_bin(obj.data)
+
     return obj
 
 
@@ -170,173 +140,133 @@ def searchForStringTypeValues(obj):
 def parse_wad(wad_path: str) -> bytes:
     wad_file = WAD()
     wad_file.read(wad_path)
-    chunk_datas = []
-    chunk_hashes = []
-    with wad_file.stream(wad_path, 'rb+') as bs:
+    chunks_dict = {}
+
+    with wad_file.stream(wad_path, "rb+") as bs:
         filesInWad.clear()
         for chunk in wad_file.chunks:
             chunk.read_data(bs)
-            if chunk.extension == 'dds' or chunk.extension == 'tex':
+            if chunk.extension in ["dds", "tex"]:
                 filesInWad.add(chunk.hash)
+
         for chunk in wad_file.chunks:
             chunk.read_data(bs)
-            if chunk.extension == 'bin':
+            if chunk.extension == "bin":
                 try:
                     bin_file = BIN()
-                    bin_file.read(path='', raw=chunk.data)
+                    bin_file.read(path="", raw=chunk.data)
                     bin_file = parse_bin(chunk.hash, bin_file)
-                    chunk.data = bin_file.write(path='', raw=True)
+                    chunk.data = bin_file.write(path="", raw=True)
                 except Exception:
                     print(f'File Hash: "{chunk.hash}" THROWN AN EXCEPTION')
-            
-            chunk_datas.append(chunk.data)
-            chunk_hashes.append(chunk.hash)
+
+            chunks_dict[chunk.hash] = chunk.data
 
     wad = WAD()
-    wad.chunks = [WADChunk.default() for _ in range(len(chunk_hashes))]
-    
-    with wad.stream('', 'rb+', raw=wad.write('', raw=True)) as bs:
-        for id, chunk in enumerate(wad.chunks):
-            chunk.write_data(bs, id, chunk_hashes[id], chunk_datas[id], previous_chunks=(
-                                wad.chunks[i] for i in range(id)))
-            chunk.free_data()
+    wad.chunks = [WADChunk.default() for _ in range(len(chunks_dict))]
+
+    with wad.stream("", "rb+", raw=wad.write("", raw=True)) as bs:
+        for idx, (chunk_hash, chunk_data) in enumerate(chunks_dict.items()):
+            wad.chunks[idx].write_data(
+                bs,
+                idx,
+                chunk_hash,
+                chunk_data,
+                previous_chunks=(wad.chunks[i] for i in range(idx))
+            )
+            wad.chunks[idx].free_data()
+
         final_bytes = bs.raw()
 
     return final_bytes
 
 def parse_fantome(fantome_path: str) -> None:
     with open(fantome_path, 'rb') as file:
-        
         zip_file = ZipFile(file, 'r')
         zip_name_list = zip_file.namelist()
 
-        has_bin_files_flag = False
-        bins_dict, final_bins_dict = {}, {}
-        for info in zip_file.infolist():
-            if not info.is_dir():
-                if info.filename.lower().endswith('.bin'):
-                    bins_dict[info.filename] = zip_file.read(info)
-                    has_bin_files_flag = True
+        # Check if there's at least a wad file to parse
+        if (not any(x.lower().endswith('.wad.client') for x in zip_name_list)):
+            # if doesnt include "META/info.json"
+            print("No WADs found in this fantome.")
+            zip_file.close()
+            return
 
-        if not has_bin_files_flag:
-            # checking for .wad.client files if doesnt havae bin files inside
-            if not any(x.lower().endswith('info.json') for x in zip_name_list) or \
-            not any(x.lower().endswith('.wad.client') for x in zip_name_list):
-                print("The Zip File does not contains info.json or .wad.client files (it isn't a fantome)")
-                return
-        
-        wads_dict, final_wads_dict, extra_infos = {}, {}, {}  # "Path/To/Wad.wad" = WadBytes
-        
+        wads_dict, final_wads_dict, extra_files = {}, {}, {}
+
         for name in zip_name_list:
             if name.lower().endswith('.wad.client'):
                 with zip_file.open(name, 'r') as f:
                     wads_dict[name] = f.read()
             else:
-                if name.lower().endswith('.bin'):
-                    continue
                 with zip_file.open(name, 'r') as f:
-                    extra_infos[name] = f.read()
-    
-    for bin_path, bin_byte in bins_dict.items():
-        try:
-            bin_file = BIN()
-            bin_file.read(path='', raw=bin_byte)
-            bin_file = parse_bin(bin_path, bin_file)
-            final_bins_dict[bin_path] = bin_file.write(path='', raw=True)
-        except Exception:
-            print(f"Bin File: {bin_path} THROWN AN EXCEPTION")
+                    extra_files[name] = f.read()
+                    if name.endswith('META/info.json'):
+                        data = json.loads(extra_files[name])
+                        Name = data['Name']
+                        author = data['Author']
+                        print(f'Fixing "{Name}" by "{author}"')
 
-    for wad_name, wad_byte in wads_dict.items():
-        filesInWad.clear()
-        wad = WAD()
-        wad.read(path='blank-path', raw=wad_byte)
-        chunk_datas = []
-        chunk_hashes = []
-        with wad.stream(path='', mode='', raw=wad_byte) as bs:
-            for chunk in wad.chunks:
-                chunk.read_data(bs)
-                # print(f"chunk ext: {chunk.hash}")
-                if chunk.extension == 'dds' or chunk.extension == 'tex':
-                    filesInWad.add(chunk.hash)
-            for chunk in wad.chunks:
-                chunk.read_data(bs)
-                # print(f"chunk ext: {chunk.hash}")
-                if chunk.extension == 'bin':
-                    try:
-                        bin_file = BIN()
-                        bin_file.read(path='', raw=chunk.data)
-                        bin_file = parse_bin(chunk.hash, bin_file)
-                        chunk.data = bin_file.write(path='', raw=True)
-                    except Exception:
-                        print(f'File Hash: "{chunk.hash}" THROWN AN EXCEPTION')
-                chunk_datas.append(chunk.data)
-                chunk_hashes.append(chunk.hash)
-
-        print(f"files: {len(filesInWad)}")
-        wad = WAD()
-        wad.chunks = [WADChunk.default() for _ in range(len(chunk_hashes))]
-        
-        with wad.stream('', 'rb+', raw=wad.write('', raw=True)) as bs:
-            for id, chunk in enumerate(wad.chunks):
-                chunk.write_data(bs, id, chunk_hashes[id], chunk_datas[id], previous_chunks=(
-                                    wad.chunks[i] for i in range(id)))
-                chunk.free_data()
-                    
-            final_wads_dict[wad_name] = bs.raw()
+    # Reuse parse_wad by writing the WAD bytes to a temp file
+    for wad_name, wad_bytes in wads_dict.items():
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".wad.client") as tmp_file:
+            tmp_file.write(wad_bytes)
+            tmp_file.flush()
+            final_wads_dict[wad_name] = parse_wad(tmp_file.name)
 
     final_zip_buffer = BytesIO()
     final_zip_file = ZipFile(final_zip_buffer, 'w', ZIP_DEFLATED, False)
 
     for final_wad_name, final_wad_bytes in final_wads_dict.items():
         final_zip_file.writestr(final_wad_name, final_wad_bytes)
-    for extra_name, extra_byte in extra_infos.items():
+    for extra_name, extra_byte in extra_files.items():
         final_zip_file.writestr(extra_name, extra_byte)
-    for final_bin_name, final_bin_byte in final_bins_dict.items():
-        final_zip_file.writestr(final_bin_name, final_bin_byte)
+        \
 
+    
     zip_file.close()
     final_zip_file.close()
 
     with open(fantome_path, 'wb') as file:
         print(f"Writing Fantome: {fantome_path}")
         file.write(final_zip_buffer.getvalue())
-        
-if path.isfile(inpt) and inpt.endswith('.bin'):
+ 
+if path.isfile(input_path) and input_path.endswith('.bin'):
     # User are using a .bin file
     try:
         bin_file = BIN()
-        bin_file.read(inpt)
-        bin_file = parse_bin(inpt, bin_file)
+        bin_file.read(input_path)
+        bin_file = parse_bin(input_path, bin_file)
         print("Writing .bin file :D")
-        bin_file.write(inpt)
+        bin_file.write(input_path)
         print("End of Script.")
     except Exception as e:
         print(e, '\nSomething went wrong')
         input()
 
-elif path.isfile(inpt) and inpt.endswith('.wad.client'):
+elif path.isfile(input_path) and input_path.endswith('.wad.client'):
     # User are using a .wad file
     try:
-        print(f"Parsing Wad: {inpt}...")
-        wad_bytes = parse_wad(inpt)
+        print(f"Parsing Wad: {input_path}...")
+        wad_bytes = parse_wad(input_path)
         print("Writing .wad file :D")
-        with open(inpt, 'wb') as f:
+        with open(input_path, 'wb') as f:
             f.write(wad_bytes)
     except Exception as e:
         print(e, '\nSomething went wrong')
         input()
 
-elif path.isfile(inpt) and (inpt.endswith('.zip') or inpt.endswith('.fantome')):
+elif path.isfile(input_path) and (input_path.endswith('.zip') or input_path.endswith('.fantome')):
     # User are using a fantome
     try:
-        print(f"Parsing Fantome: {inpt}")
-        parse_fantome(inpt)
+        print(f"Parsing Fantome: {input_path}")
+        parse_fantome(input_path)
         print("End of Script.")
     except Exception as e:
         print(e, '\nSomething went wrong')
         input()
 
-elif path.isdir(inpt):
+elif path.isdir(input_path):
     # User are using a dir
     from os import walk
     found_bins = []
@@ -344,7 +274,7 @@ elif path.isdir(inpt):
     found_fantomes = []
     print("Searching for Wads and Bins and Fantomes/Zips inside the desired folder.")
     
-    for root, dirs, files in walk(inpt):
+    for root, dirs, files in walk(input_path):
         for file in files:
             if file.lower().endswith('.bin'):
                 found_bins.append(path.join(root, file))
@@ -387,4 +317,3 @@ elif path.isdir(inpt):
 else:
     print("Couldn't guess the desired object to fix")
     input()
-    
